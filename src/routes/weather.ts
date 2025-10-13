efb484731fa2bcb69372a95208aed2988845696f
kevin.rowe <kevin.rowe@marketnode.com>
2025-10-13 14:49:47 +0800

    Add date filter endpoint

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -23,4 +23,36 @@ router.get('/', async (_: Request, res: Response<CommonResponse<Weather[]>>) => 
   }
 })
 
+router.get('/:date', async (req: Request, res: Response<CommonResponse<Weather[]>>) => {
+    try {
+    const {date} = req.params
+    const params = []
+    const dateRegex = /\d{4,4}\-\d{2,2}\-\d{2,2}/
+    let sql = `SELECT * FROM weather`
+
+    if (!date || !dateRegex.test(date)) {
+      res.status(400).json({
+        error: 'Invalid date provided',
+        data: null
+      })
+      return
+    }
+
+    sql += " WHERE date(timestamp) = ?"
+    params.push(date)
+
+    const allWeather = await query<Weather>(sql, params)
+
+    res.status(200).json({
+      error: null,
+      data: allWeather
+    })
+  } catch (e: unknown) {
+    res.status(500).json({
+      error: e instanceof Error ? e.message : String(e),
+      data: null
+    })
+  }
+})
+
 export default router;
